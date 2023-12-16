f0c4618f20fda7564d4f1d2dbb9cd748abd49fc1
lakshmiprasadlp <145165461+lakshmiprasadlp@users.noreply.github.com>
2023-12-16 20:18:38 +0530

    Update tcp port .py

--- app.py
+++ app.py
@@ -26,5 +26,6 @@ def predict():
         return render_template("index.html", result=result)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
+
